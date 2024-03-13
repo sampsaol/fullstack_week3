907bb1f0b0af668fa95158b1482142bb83d0e1ee
Sampsa Ollikainen <sampsa.ollikainen@gmail.com>
2024-03-13 18:10:34 +0200

    uibuild

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ morgan.token('b-content', (request) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b-content'))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
   {
